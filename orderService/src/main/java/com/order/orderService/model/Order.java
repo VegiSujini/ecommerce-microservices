db80d6ab47c3c0938cc3cead0f13fff11798ec27
VegiSujini <sujini.vegi@gmail.com>
2024-08-15 17:21:20 +0530

    added table names

--- orderService/src/main/java/com/order/orderService/model/Order.java
+++ orderService/src/main/java/com/order/orderService/model/Order.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "Order")
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
