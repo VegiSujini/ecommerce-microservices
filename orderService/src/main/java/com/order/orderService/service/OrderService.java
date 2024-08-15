62e4036d3a241b6258afbd1fc44eb82cb2894bb1
VegiSujini <sujini.vegi@gmail.com>
2024-08-15 16:54:14 +0530

    added order service file to communicate order and product

--- orderService/src/main/java/com/order/orderService/service/OrderService.java
+++ orderService/src/main/java/com/order/orderService/service/OrderService.java
@@ -1,9 +1,27 @@
 package com.order.orderService.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.order.orderService.client.ProductClient;
+import com.order.orderService.model.Order;
+import com.order.orderService.model.Product;
+import com.order.orderService.repository.OrderRepository;
 
 @Service
 public class OrderService {
-    
-    private OrderR
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ProductClient productClient;
+
+    public Order createOrder(Long productId, int quantity) {
+        Product product = productClient.getProductById(productId);
+        Order order = new Order();
+        order.setProductId(productId);
+        order.setQuantity(quantity);
+        order.setTotalPrice(product.getPrice() * quantity);
+        return orderRepository.save(order);
+    }
 }
