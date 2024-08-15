08c25dfaba0b53ed5685a28b0dbb79240bedc3e0
VegiSujini <sujini.vegi@gmail.com>
2024-08-15 15:10:40 +0530

    added service file

---
+++ productService/src/main/java/com/productservice/productService/service/ProductService.java
@@ -0,0 +1,34 @@
+package com.productservice.productService.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.productservice.productService.model.Product;
+import com.productservice.productService.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+        ;
+    }
+}
