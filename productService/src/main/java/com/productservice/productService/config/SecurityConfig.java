5755ec2d7679de596e6bd3a27aea63818402df1f
VegiSujini <sujini.vegi@gmail.com>
2024-08-19 21:31:41 +0530

    updated the pom file

--- productService/src/main/java/com/productservice/productService/config/SecurityConfig.java
+++ productService/src/main/java/com/productservice/productService/config/SecurityConfig.java
@@ -1,5 +1,27 @@
 package com.productservice.productService.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
-    
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.antMatchers("/public/**").permitAll()
+                .anyRequest().authenticated()).oauth2ResourceServer(
+                        oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        return converter;
+    }
 }
